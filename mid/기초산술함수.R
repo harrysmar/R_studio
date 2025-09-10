03816bf38daaba1b8e12a01d326ddd5a75621a5a
ken <limstar9147@gmail.com>
2025-09-10 21:36:04 +0900

    Update 기초산술함수.R

--- mid/기초산술함수.R
+++ mid/기초산술함수.R
@@ -1,1 +1,60 @@
-# --- 1. 통계함수 ---
+# --- 1. 통계 함수 ---
+# sum()은 
+# mean()은
+# var()은
+# sd()는
+#
+#
+#
+#
+#
+sum
+mean
+var
+sd
+median
+max
+min
+IQR
+quantile(, probs)
+
+range
+
+summary
+
+fivenum
+
+# --- 2. 수학 함수 ---
+## --- 2.1. 로그 및 지수 함수
+# 
+# log( , base=exp())
+log
+log10
+log2
+log
+exp
+exp
+
+## --- 2.2. 삼각 함수 ---
+cos
+sin
+tan
+acos
+asin
+atan
+
+## --- 2.3. 기타 수학 함수 ---
+pi
+abs
+sqrt
+sign
+round
+ceiling
+floor
+trunc
+singif
+factorial
+choose
+gamma
+beta
+options
